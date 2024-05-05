5e18817489ade2b96ca2b1fce0cd9247b9fb40cf
kssseniya <kseniyag2005@gmail.com>
2024-05-05 19:27:22 +0300

    added name

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <string>
 using namespace std;
-int main()
-{
-  cout << "Hello world!" << endl;
+int main() {
+  string name;
+  cout << "Enter your name:" << endl;
+  cin >> name;
+  cout << "Hello world from " << name << endl;
   return 0;
 }
