8d66b3e2f3445dcb4214c607b9316359ddf5abb4
kssseniya <kseniyag2005@gmail.com>
2024-05-05 20:54:06 +0300

    with comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
-
+//функция
 int main() {
   string name;
   cout << "Enter your name:" << endl;
-  cin >> name;
+  cin >> name; //ввод имени
   cout << "Hello world from " << name << endl;
   return 0;
 }
